b40f9ce301981260eb32b7ff2187af23b79b1b21
Jyejye99 <kjh970324@naver.com>
2023-04-03 19:18:04 +0900

    카테고리별 상품 뿌리기 완료

--- start.js
+++ start.js
@@ -20,14 +20,16 @@ con.connect( err => {
         let sqlGetMenuInProduct= `select * from v_menu_in_product;`;
         let sqlGetProduct= `select * from v_product;`;
         let sqlGetProductOption= `select * from v_product_option;`;
+        let menuCategory= `select id as lv1_id, name as lv1_name from menu where pid is null;`;
 
-        con.query( sqlGetMenu + sqlGetMenuInProduct + sqlGetProduct + sqlGetProductOption, ( err, result ) => {
+        con.query( sqlGetMenu + sqlGetMenuInProduct + sqlGetProduct + sqlGetProductOption + menuCategory, ( err, result ) => {
             if( err ) throw err;
             res.render( `index`, {
                 menu: result[0],
                 menuInProduct: result[1],
                 product: result[2],
-                productOption: result[3]
+                productOption: result[3],
+                menuCategory: result[4]
             } );
         });
     });
