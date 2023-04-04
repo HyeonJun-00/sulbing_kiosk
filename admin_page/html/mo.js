9e0ddca82e763ad4df310ca00bb36c4751314a55
HyeonJun <fnguswns1@naver.com>
2023-04-04 20:09:12 +0900

    관리자 페이지 3

--- admin_page/html/mo.js
+++ admin_page/html/mo.js
@@ -1,10 +1,20 @@
 (() => {
     const   mainMenu = $(".mainMenu li");
+    const   productInventory = $(".productInventory > div button");
 
     for (i = 1; i < mainMenu.length; i++) {
         mainMenu.eq(i).on("click", function () {
+            mainMenu.removeClass("backgroundFlag");
+            $(this).addClass("backgroundFlag");
             $(".mainDisplay section").removeClass("displayFlag");
             $(".mainDisplay section").eq($(this).index() - 1).addClass("displayFlag");
+        });
+    }
+    console.log(productInventory.length);
+    for (i = 0; i < productInventory.length; i++) {
+        productInventory.eq(i).on("click", function () {
+            productInventory.removeClass("borderFlag");
+            $(this).addClass("borderFlag");
         });
     }
 })();
