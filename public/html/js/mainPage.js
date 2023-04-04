7144676e1d846c0c252d73f9f6b63df0bee53927
Jyejye99 <kjh970324@naver.com>
2023-04-04 13:39:08 +0900

    수정

--- public/html/js/mainPage.js
+++ public/html/js/mainPage.js
@@ -1,5 +1,7 @@
-
-function fn_showTopping (productId) {
-    document.getElementById('asd').style.display = 'block';
-    document.getElementById('selectTopp').src = '/selectTopping?productId=' + productId; 
+function inputElement() {
+    let showModal = document.querySelector('#toppModalBack');
+    showModal.style.display = 'block';
+    let menuName = document.querySelector('.mProd_name');
+    let inputName = document.querySelector('.prod_name').textContent;
+    menuName.innerHTML = inputName;
 }
