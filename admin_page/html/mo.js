701388efad1630d512c27baa46093a7c97563526
HyeonJun <fnguswns1@naver.com>
2023-04-05 11:31:44 +0900

    a

--- admin_page/html/mo.js
+++ admin_page/html/mo.js
@@ -1,13 +1,13 @@
 (() => {
     const   mainMenu = $(".mainMenu li");
-    const   productInventory = $(".productInventory > div button");
+    const   productInventory = $(".productInventory div:first-child > button");
 
     for (i = 1; i < mainMenu.length; i++) {
         mainMenu.eq(i).on("click", function () {
             mainMenu.removeClass("backgroundFlag");
             $(this).addClass("backgroundFlag");
-            $(".mainDisplay section").removeClass("displayFlag");
-            $(".mainDisplay section").eq($(this).index() - 1).addClass("displayFlag");
+            $(".mainDisplay > section").removeClass("displayFlag");
+            $(".mainDisplay > section").eq($(this).index() - 1).addClass("displayFlag");
         });
     }
     console.log(productInventory.length);
@@ -15,6 +15,8 @@
         productInventory.eq(i).on("click", function () {
             productInventory.removeClass("borderFlag");
             $(this).addClass("borderFlag");
+            $(".productInventory div").removeClass("displayFlag");
+            $(".productInventory div").eq($(this).index() + 1).addClass("displayFlag");
         });
     }
 })();
