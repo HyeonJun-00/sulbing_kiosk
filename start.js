6d1835f47cbabfb6c24cbc7f2533a30af9c40123
minhee <miniwooeng@gmail.com>
2023-04-06 10:15:02 +0900

    routing

--- start.js
+++ start.js
@@ -15,41 +15,12 @@ app.use( express.static( `./public/html` ) );
 
 const port= 4191;
 
-app.get( `/`, ( req, res ) => {
-    let sqlGetMenu= `select * from v_menu;`;
-    let sqlGetMenuInProduct= `select * from v_menu_in_product;`;
-    let sqlGetProduct= `select * from v_product;`;
-    let sqlGetProductOption= `select * from v_product_option;`;
-    let menuCategory= `select id as lv1_id, name as lv1_name from menu where pid is null and deleted_date is null;`;
-    let optionCategory= `SELECT distinct product_id, option_category FROM line_sulbing_kiosk.v_product_option order by 1, 2;`
-    con.query( sqlGetMenu + sqlGetMenuInProduct + sqlGetProduct + sqlGetProductOption + menuCategory + optionCategory, ( err, result ) => {
-        if( err ) throw err;
-        res.render( `index`, {
-            menu: result[0],
-            menuInProduct: result[1],
-            product: result[2],
-            productOption: result[3],
-            menuCategory: result[4],
-            optionCategory: result[5]
-        } );
-    });
-});
-
-app.get ( `/admin_menu`, ( req, res, next ) => {
-    res.render( `adminMenu` );
-});
-app.get ( `/admin_order`, ( req, res, next ) => {
-    res.render(`adminOrder`);
-});
-app.get ( `/admin_product`, ( req, res, next ) => {
-    res.render(`adminProduct`);
-});
-app.get ( `/admin_sales`, ( req, res, next ) => {
-    res.render(`adminSales`);
-});
-app.get ( `/admin_user`, ( req, res, next ) => {
-    res.render(`adminUser`);
-});
+app.use( `/`, indexRouter );
+app.use( `/admin_menu`, adminMenuRouter );
+app.use( `/admin_order`, adminOrderRouter );
+app.use( `/admin_product`, adminProductRouter );
+app.use( `/admin_sales`, adminSalesRouter );
+app.use( `/admin_user`, adminUserRouter );
 
 app.listen( port, () => {
     console.log( `connected ${ port } port!` );
