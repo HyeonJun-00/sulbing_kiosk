9f56587db0adebb7fcaacca921e354609061a7a7
repeeps <seoji1986@gmail.com>
2023-04-13 16:40:34 +0900

    check admin sass

--- routes/admin/menu.js
+++ routes/admin/menu.js
@@ -18,34 +18,46 @@ router.get( `/`, ( req, res, next ) => {
 });
 
 router.post('/menuCheck', (req, res)=>{
-    console.log(req.body.checkMenu, req.body.nameMenu);
+    console.log(req.body.checkMenu, req.body.nameMenu, req.body.isMain);
     let sqlActive = `UPDATE menu SET deleted_date = NULL WHERE name Like '${req.body.nameMenu}'`;
     let sqlInactive = `UPDATE menu SET deleted_date = now() WHERE name Like '${req.body.nameMenu}'`;
     let sqlCheck = `SELECT deleted_date FROM menu WHERE name Like '${req.body.nameMenu}'`;
-    let sqlGetMenu= `SELECT * FROM menu WHERE pid IS NULL;`;
-
+    let sqlInactiveSub= `UPDATE menu SET deleted_date = now() WHERE pid Like '${req.body.nameMenu}'`;
 
 
     switch(req.body.checkMenu){
 
         case "active":
-            con.query(sqlActive , (err, result)=>{
-                if(err) throw err;
+            con.query(sqlActive, (err, result) => {
+                if (err) throw err;
             });
             break;
 
         case "inactive":
-            con.query(sqlCheck, (err, checker)=>{
-                if(err) throw err;
 
-                if(checker[0].deleted_date == null){
-                    con.query(sqlInactive , (err, result)=>{
-                        if(err) throw err;
+            if(req.body.isMain == 'true'){
+                con.query(sqlInactive, (err, result)=>{
+                    if (err) throw err;
+                    console.log(req.body.nameMenu);
+
+                    con.query(sqlInactiveSub, (err, result2)=>{
+                        console.log(sqlInactiveSub);
+                        if (err) throw err;
                     });
-                } else{
-                    console.log('already inactivated!!!');
-                }
-            });
+                });
+            } else {
+                con.query(sqlCheck, (err, checker) => {
+                    if (err) throw err;
+
+                    if (checker[0].deleted_date == null) {
+                        con.query(sqlInactive, (err, result) => {
+                            if (err) throw err;
+                        });
+                    } else {
+                        console.log('already inactivated!!!');
+                    }
+                });
+            }
             break;
     }
     res.redirect('/admin_menu');
