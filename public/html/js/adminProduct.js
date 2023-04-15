44abe1191919fdcfe349c0c98fd6562b0c297395
minhee <miniwooeng@gmail.com>
2023-04-15 16:30:12 +0900

    상품관리 페이지 제작중...

--- public/html/js/adminProduct.js
+++ public/html/js/adminProduct.js
@@ -1,1 +1,29 @@
-console.log('aaa');
+( loadJs= () => {
+
+    document.querySelector( `.searchResetBtn` ).onclick= () => {
+        [...document.querySelectorAll( `.productSearchRow input` )].forEach( v => {
+            v.value= '';
+        });
+    }
+
+    [...document.querySelectorAll( `.menuSet li` )].forEach( ( v, i, a ) => {
+        v.onclick = e => {
+            if( e.target.hasAttribute( `data-lv1-id` ) ) {
+                a.forEach( v1 => {
+                    v1.classList.remove( `active` );
+                });
+                e.target.classList.add( `active` );
+                document.querySelector( `.productSearchRow [name=menuLv1Name]` ).value= e.target.innerHTML;
+                [...document.querySelectorAll( `.menuLv2` )].forEach( ( v2, i2 ) => {
+                    if( i == i2 ) { v2.classList.add( `active` );
+                    } else {
+                        v2.classList.remove( `active` );
+                    }
+                })
+            } else {
+                document.querySelector( `.productSearchRow [name=menuLv2Name]` ).value= e.target.innerHTML;
+            }
+        }
+    });
+
+})();
