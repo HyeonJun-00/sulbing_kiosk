6c7a9f7260c9c04b8e716c5e6f1ce73a6cda7f7d
repeeps <seoji1986@gmail.com>
2023-04-17 18:55:29 +0900

    done for now

--- public/html/js/adminSale.js
+++ public/html/js/adminSale.js
@@ -1,6 +1,7 @@
 $(document).ready(()=>{
 
     $('.displayMonthlySale').css('display','flex');
+    $('#monthlySale').css('background-color', '#f3f3f3');
     let heightValue = [];
     let heightMax = 0;
     $(".dataValue").each((i,v)=>{
@@ -11,6 +12,7 @@ $(document).ready(()=>{
     $(".dataValue").each((i,v)=>{
        let heightV = Number(v.getAttribute('data-price'));
         v.style.height = `${(heightV/heightMax)*100}%`;
+        v.style.backgroundColor = `lightblue`;
     });
     heightValue = [];
     $(".dayValue").each((i,v)=>{
@@ -21,6 +23,7 @@ $(document).ready(()=>{
     $(".dayValue").each((i,v)=>{
         let heightV = Number(v.getAttribute('data-price'));
         v.style.height = `${(heightV/heightMax)*100}%`;
+        v.style.backgroundColor = `#ffcb6b`;
     });
 
     heightMax = Math.max( Number($('.yearValue').attr('data-price')), Number($('.lastYearValue').attr('data-price')));
@@ -28,10 +31,12 @@ $(document).ready(()=>{
     $(".yearValue").each((i,v)=>{
         let heightV = Number(v.getAttribute('data-price'));
         v.style.height = `${(heightV/heightMax)*100}%`;
+        v.style.backgroundColor = `rgba(255,0,0,0.4)`;
     });
     $(".lastYearValue").each((i,v)=>{
         let heightV = Number(v.getAttribute('data-price'));
         v.style.height = `${(heightV/heightMax)*100}%`;
+        v.style.backgroundColor = `rgba(255,0,0,0.2)`;
     });
 
     heightValue = [];
@@ -52,32 +57,48 @@ $(document).ready(()=>{
                 $('.displayDailySale').css('display', 'none');
                 $('.displayYearlySale').css('display', 'none');
                 $('.displayBestSales').css('display', 'none');
+                $('#dailySale').css('background-color', 'lightgray');
+                $('#yearlySale').css('background-color', 'lightgray');
+                $('#bestMenu').css('background-color', 'lightgray');
                 if($('.displayMonthlySale').css('display') === 'none'){
                     $('.displayMonthlySale').css('display','flex');
+                    $('#monthlySale').css('background-color', '#f3f3f3');
                 }
                 break;
             case 'dailySale' :
                 $('.displayMonthlySale').css('display', 'none');
                 $('.displayYearlySale').css('display', 'none');
                 $('.displayBestSales').css('display', 'none');
+                $('#monthlySale').css('background-color', 'lightgray');
+                $('#yearlySale').css('background-color', 'lightgray');
+                $('#bestMenu').css('background-color', 'lightgray');
                 if($('.displayDailySale').css('display') === 'none') {
                     $('.displayDailySale').css('display', 'flex');
+                    $('#dailySale').css('background-color', '#f3f3f3');
                 }
                 break;
             case 'yearlySale' :
                 $('.displayDailySale').css('display', 'none');
                 $('.displayMonthlySale').css('display', 'none');
                 $('.displayBestSales').css('display', 'none');
+                $('#monthlySale').css('background-color', 'lightgray');
+                $('#dailySale').css('background-color', 'lightgray');
+                $('#bestMenu').css('background-color', 'lightgray');
                 if($('.displayYearlySale').css('display') === 'none') {
                     $('.displayYearlySale').css('display', 'flex');
+                    $('#yearlySale').css('background-color', '#f3f3f3');
                 }
                 break;
             case 'bestMenu' :
                 $('.displayDailySale').css('display', 'none');
                 $('.displayMonthlySale').css('display', 'none');
                 $('.displayYearlySale').css('display', 'none');
+                $('#monthlySale').css('background-color', 'lightgray');
+                $('#yearlySale').css('background-color', 'lightgray');
+                $('#dailySale').css('background-color', 'lightgray');
                 if($('.displayBestSales').css('display') === 'none') {
                     $('.displayBestSales').css('display', 'flex');
+                    $('#bestMenu').css('background-color', '#f3f3f3');
                 }
                 break;
         }
--- routes/index.js
+++ routes/index.js
@@ -226,7 +226,7 @@ router.post ( `/login`, async ( req, res ) => {
             console.log(v.tel);
             (v.tel == pwGet) && (flagPW = true)
         });
-        if(flagPW != true) {
+        if(flagPW == true) {
             res.redirect('/admin_order');
         } else {
             res.redirect('/');
